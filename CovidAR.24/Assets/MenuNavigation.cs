63bf42ad0e8e7b5a3322cc441faef20b09d31c9f
Jessela <47195699+Jessela26@users.noreply.github.com>
2020-03-30 22:45:50 -0400

    Imported important packages

---
+++ CovidAR.24/Assets/MenuNavigation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNavigation : MonoBehaviour
+{
+    public void StartGame()
+    {
+        Debug.Log("Game Started!");
+        SceneManager.LoadScene("MainInterface");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
+    }
+}
